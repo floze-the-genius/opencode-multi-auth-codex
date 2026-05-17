a9826e8c75a6a24ebd7c11412b22fd873883923d
EremesNG <er3mes@gmail.com>
2026-05-17 14:02:49 -0600

    chore(dependencies): remove package-lock.json file

--- dist/web.d.ts
+++
@@ -1,7 +0,0 @@
-import * as http from 'node:http';
-export declare function isLocalhostHost(host: string): boolean;
-export declare function startWebConsole(options?: {
-    port?: number;
-    host?: string;
-}): http.Server;
-//# sourceMappingURL=web.d.ts.map
